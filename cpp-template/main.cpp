aab249c2bd43bfb621d032511cd8614f3c777b70
Matthew Close <ikea48@outlook.com>
2025-07-22 00:54:51 -0400

    Make this shit exist.

---
+++ cpp-template/main.cpp
@@ -0,0 +1,8 @@
+// Template C++ program
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << 3 + 3;
+  return 0;
+}
+
+int myNumber;
+int myNumber = 42; // Example of a variable declaration and initialization
